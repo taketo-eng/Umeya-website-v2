58e820e2fc6b300743b4ff2aca73af114df83af3
Taketo Yahiro (Yapimaru) <yahiro.t.eng@gmail.com>
2026-02-10 23:55:51 +0900

    create header and footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+          @use "/src/styles/global" as g;
+          @use "/src/styles/foundation" as *;
+          @use "/src/styles/layout" as *;
+          `,
+        }
+      }
+    }
+  }
+});
