323c320378785a736ee384a1a0eccbbe6024c73d
Taketo Yahiro (Yapimaru) <yahiro.t.eng@gmail.com>
2026-04-25 20:31:46 +0900

    add past-events

--- src/lib/umeyui.ts
+++ src/lib/umeyui.ts
@@ -1,14 +1,21 @@
 import type { Event, Vendor } from "./types/umeyui"
 
 export const getEvents = async (): Promise<Event[]> => {
-  const response = await fetch(`${import.meta.env.BASE_URL}public/events`)
+  const response = await fetch(`${import.meta.env.UMEYUI_BASE_URL}public/events`)
+
+  const events: Event[] = await response.json()
+  return events
+}
+
+export const getPastEvents = async (): Promise<Event[]> => {
+  const response = await fetch(`${import.meta.env.UMEYUI_BASE_URL}public/past-events`)
 
   const events: Event[] = await response.json()
   return events
 }
 
 export const getVendors = async (): Promise<Vendor[]> => {
-  const response = await fetch(`${import.meta.env.BASE_URL}public/vendors`)
+  const response = await fetch(`${import.meta.env.UMEYUI_BASE_URL}public/vendors`)
 
   const vendors: Vendor[] = await response.json()
   return vendors
